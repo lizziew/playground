eabc7a0765892d22d5cc8a54ab8ab47e38fe7828
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2016-01-25 23:18:00 -0500

    pipe and condensed exception

--- kdb_constitution/program.q
+++ kdb_constitution/program.q
@@ -2,3 +2,4 @@ strip:{x except "[]():,;.-0123456789"} / function to take away punctuation, numb
 us: strip each lower raze " " vs/: read0`:input.txt / take away punctuation, numbers, turn into lowercase 
 s: desc count each group us except enlist "" / count word frequency and sort in descending order
 `:solution.txt 0: (key s) ,' "|" ,' (string value s) / output to pipe delimited text file
+\\
--- kdb_constitution/wordfreq.java
+++ kdb_constitution/wordfreq.java
@@ -12,45 +12,40 @@ public class wordfreq {
 		}
 
 		public String toString() {
-			return w + " " + c; 
+			return w + "|" + c; 
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		HashMap<String, Integer> freq = new HashMap<String, Integer>();
-		try {
-			Scanner s = new Scanner(new File("input.txt"));
-			while (s.hasNext()){
-				String str = s.next().replaceAll("[^a-zA-Z ]", "").toLowerCase();
-				if(!str.equals("")) {
-					if(freq.containsKey(str)) {
-						freq.put(str, freq.get(str)+1); 
-					}
-					else {
-						freq.put(str, 1); 
-					}
+		Scanner s = new Scanner(new File("input.txt"));
+		while (s.hasNext()){
+			String str = s.next().replaceAll("[^a-zA-Z ]", "").toLowerCase();
+			if(!str.equals("")) {
+				if(freq.containsKey(str)) {
+					freq.put(str, freq.get(str)+1); 
+				}
+				else {
+					freq.put(str, 1); 
 				}
 			}
-			
-			List<word> list = new ArrayList<word>(); 
-			for (String w: freq.keySet()) { 
-	            list.add(new word(w, freq.get(w)));
-			} 
-
-			Collections.sort(list, new Comparator<word>() {
-				public int compare(word w, word o) {
-					return o.c - w.c; 
-				}
-			});
-
-			PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
-			System.setOut(out);
-
-			for(int i = 0; i < list.size(); i++)			
-				System.out.println(list.get(i));
 		}
-		catch(FileNotFoundException e) {
-			System.out.println(e); 
-		}
+		
+		List<word> list = new ArrayList<word>(); 
+		for (String w: freq.keySet()) { 
+            list.add(new word(w, freq.get(w)));
+		} 
+
+		Collections.sort(list, new Comparator<word>() {
+			public int compare(word w, word o) {
+				return o.c - w.c; 
+			}
+		});
+
+		PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+		System.setOut(out);
+
+		for(int i = 0; i < list.size(); i++)			
+			System.out.println(list.get(i));
 	}
 }
