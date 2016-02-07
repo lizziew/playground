0106bb6a94d4bd6ccdb50486f95c1f6cc4c8f610
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2016-02-07 13:44:07 -0500

    handle large nums

--- algorithms/misc/fibonacci.py
+++ algorithms/misc/fibonacci.py
@@ -1,5 +1,11 @@
-import numpy as np
 import time
+
+def dot(x, y):
+    a=x[0][0]*y[0][0]+x[1][0]*y[0][1]
+    b=x[0][0]*y[1][0]+x[1][0]*y[1][1]
+    c=x[1][0]*y[0][0]+x[1][1]*y[0][1]
+    d=x[1][0]*y[1][0]+x[1][1]*y[1][1]
+    return [[a,b],[c,d]]
 
 #repeated squaring, matrix multiplication
 def matrixfib(n): 
@@ -9,11 +15,11 @@ def matrixfib(n):
 		
 	while n > 0: 
 		if n % 2 == 1:
-			t = np.dot(t, a) 
+			t = dot(t, a) 
 		n /= 2
-		a = np.dot(a, a)
+		a = dot(a, a)
 
-	return np.dot(t, b)[1]
+	return t[1][0]*b[0]+t[1][1]*b[1]
 
 def naive(n):
 	if n == 0 or n ==1:
@@ -34,20 +40,19 @@ def linearfib(n):
 
 	return res 
 
-def test():
-	n = 30 
+def test(n):
 	print "n = ", n
 
 	t0 = time.time()
-	ans_matrix = matrixfib(n)
-	t1 = time.time() 
 	ans_linear = linearfib(n)
+	t1 = time.time() 
+	ans_matrix = matrixfib(n)
 	t2 = time.time() 
 	ans_naive = naive(n)
 	t3 = time.time() 
 
-	print "matrix: ", ans_matrix, "time: ", t1 - t0, " seconds"
-	print "linearfib: ", ans_linear, "time: ", t2 - t1, " seconds"
+	print "matrix: ", ans_matrix, "time: ", t2 - t1, " seconds"
+	print "linearfib: ", ans_linear, "time: ", t1 - t0, " seconds"
 	print "naive:   ", ans_naive,   "time: ", t3-t2, " seconds."
 
-test() 
+test(1000000)
